d847b12efa592a75a3345bd7a69acb89ec3f96d7
knas <olssonjoke@gmail.com>
2022-03-22 12:59:56 +0100

    Add draw status

--- components/playerList.tsx
+++ components/playerList.tsx
@@ -7,7 +7,7 @@ function PlayerList({ players=[] }: any) {
                 <div className="player" key={player.id}>
                     
                     <div className="player-name"><span>{player.name}</span> <span className="player-status">{
-                        player?.guessedCorrect ? 'Got it!' : ''
+                        player?.guessedCorrect ? 'Got it!' : (player?.isDrawing ? 'Drawing' : '');
                     }</span></div>
                     <div className="player-score">{player.score} points</div>
                 </div>)
